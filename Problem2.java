50a0c3e7268de903c935445fc99c6558cf246458
Ram Charan Kondeti <kondetiramcharan12@gmail.com>
2025-11-19 19:48:03 +0530

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Problem2 {
     public static void main(String[] args) {
-    	
-        Scanner sc= new Scanner(System.in);
+        
+        Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
         int num = 1;
@@ -13,10 +13,11 @@ public class Problem2 {
         for (int i = 0; i < a; i++) {
             System.out.print(num);
             if (i != a - 1) {
-            	System.out.print(" ");
+                System.out.print(" ");
             }
             num += 2;
         }
-        sc.close(); 
+
+        sc.close();
     }
 }
